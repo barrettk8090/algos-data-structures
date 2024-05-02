bc2dd1b212d118ab39da74ed2f4fc142db7315ac
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-05-02 08:23:47 -0600

    array rotation solution 1

--- array/array_rotation.py
+++ array/array_rotation.py
@@ -1,3 +1,51 @@
 # Array Rotation: 
+# https://www.geeksforgeeks.org/python-program-for-program-for-array-rotation-2/?ref=lbp 
 # Given an array like: [1,2,3,4,5,6,7]
 # Rotate to the following: [2,3,4,5,6,7,1]  /// Left rotation by 1 
+
+
+# First function to reverse the given array by swapping the 
+# first and last numbers 
+def reverse(start, end, arr):
+    # Number of iterations needed for reversing the list
+    no_of_reverse = end-start+1
+
+    #By incrementing count value, swapping of first and last elements is done 
+    count = 0
+    while ((no_of_reverse)//2 != count):
+        arr[start+count], arr[end-count] = arr[end-count], arr[start+count]
+        count +=1  
+    return arr 
+
+# Second function takes array, length of array, and no of rotations as input
+def left_rotate_array(arr, size, d): 
+    #Reverse the entire list
+    start = 0
+    end = size-1
+    arr = reverse(start, end, arr)
+
+    # Divide the array into two sub-arrays based on the no of rotations
+    # Divide the first sub-array, reverse the first sub-array
+
+    start = 0
+    end = size-d-1
+    arr = reverse(start, end, arr)
+
+    # Divide the second sub-array
+    # Reverse the second sub-array
+    start = size-d
+    end - size-1
+    arr = reverse(start, end, arr)
+    return arr 
+
+arr = [1,2,3,4,5,6,7,8]
+size = 8
+d = 1
+print("Original array: ", arr)
+
+# Finding all the symmetric rotation number
+if (d <= size):
+    print("Rotated array ", left_rotate_array(arr, size, d))
+else:
+    d = d % size
+    print("Rotated array: ", left_rotate_array(arr, size, d))
