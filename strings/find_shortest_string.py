4f5948bc1798ad0b7717a357b832cecce6cdb9a4
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-05-03 10:21:38 -0600

    start find shortest string

---
+++ strings/find_shortest_string.py
@@ -0,0 +1,15 @@
+# Instructions
+# Given an Array of strings, return the shortest string. If there is more than one string of that length, return the string that comes first in the list. The Array will have a minimum length of 1.
+
+        # Input: ['aaa', 'a', 'bb', 'ccc']
+        # Output: 'a'
+
+        # Input: ['cat', 'hi', 'dog', 'an']
+        # Output: 'hi'
+
+        # Input: ['flower', 'juniper', 'lily', 'dandelion']
+        # Output: 'lily'
+
+
+def find_shortest_string(arr):
+    pass
