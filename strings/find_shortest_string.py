b5984b178971d0e00fc6e7deb8dfb7766f08635a
Barrett Kowalsky <barrettkowalsky@gmail.com>
2024-05-04 08:37:26 -0600

    built in guide and find shortest string solutions

--- strings/find_shortest_string.py
+++ strings/find_shortest_string.py
@@ -10,8 +10,34 @@
         # Input: ['flower', 'juniper', 'lily', 'dandelion']
         # Output: 'lily'
 
+# Solution 1 - Using .sort(key=len) – sort is O(n log n) where n is # of elements in list
 
 def find_shortest_string(arr):
-    pass
-    # for i in arr:
-    #     if len(arr[i]) 
+    arr.sort(key=len)
+    print(arr[0])
+
+find_shortest_string(['aaa', 'a', 'bb', 'ccc'])
+find_shortest_string(['cat', 'hi', 'dog', 'an'])
+find_shortest_string(['flower', 'juniper', 'lily', 'dandelion'])
+
+# Solution 2 - O(n) where n is the number of elements in the array 
+def shortest_string(arr):
+    if not arr:
+        return None
+    
+    min_length = len(arr[0]) # Start by setting min_length to first element in array
+    min_str = arr[0]
+    
+    for i in range(1, len(arr)):
+        if len(arr[i]) < min_length: # Checking each item in the list to see if its less than arr[0]
+            min_length = len(arr[i])
+            min_str = arr[i]
+    
+    return min_str
+
+# Solution 3 - Using the built in min() method with key of length - O(n)
+def find_shortest_string_min(arr):
+    print(min(arr, key=len))
+
+find_shortest_string_min(['aaa', 'a', 'bb', 'ccc'])
+find_shortest_string_min(['cat', 'hi', 'dog', 'an'])
